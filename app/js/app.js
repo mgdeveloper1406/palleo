646598ef94ee5ecae8e266d7eb51a6b3da688cd7
John Robinson <john.robinson@wellzesta.com>
2014-09-28 11:48:19 -0500

    Initial commit

---
+++ app/js/app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/* App Module */
+
+var paleoApp = angular.module('paleoApp', [
+  'ngRoute',
+  'paleoControllers',
+  'paleoFilters',
+  'paleoServices'
+]);
+
+paleoApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/phones', {
+        templateUrl: 'partials/phone-list.html',
+        controller: 'PhoneListCtrl'
+      }).
+      when('/phones/:phoneId', {
+        templateUrl: 'partials/phone-detail.html',
+        controller: 'PhoneDetailCtrl'
+      }).
+      otherwise({
+        redirectTo: '/phones'
+      });
+  }]);
---
+++ app/js/controllers.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/* Controllers */
+
+var paleoControllers = angular.module('paleoControllers', []);
+
+paleoControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
+  function($scope, Phone) {
+    $scope.phones = Phone.query();
+    $scope.orderProp = 'age';
+  }]);
+
+paleoControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
+  function($scope, $routeParams, Phone) {
+    $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
+      $scope.mainImageUrl = phone.images[0];
+    });
+
+    $scope.setImage = function(imageUrl) {
+      $scope.mainImageUrl = imageUrl;
+    }
+  }]);
